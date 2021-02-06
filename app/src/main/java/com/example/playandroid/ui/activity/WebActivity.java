7394af9c86dc289655c43178936f68aae6c0e906
wzt <2238091763@qq.com>
2021-02-07 03:34:11 +0800

    解决net::ERR_UNKNOWN_URL_SCHEME&net::err_unknown_url_scheme错误

--- app/src/main/java/com/example/playandroid/ui/activity/WebActivity.java
+++ app/src/main/java/com/example/playandroid/ui/activity/WebActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -24,12 +25,41 @@ public class WebActivity extends AppCompatActivity {
             getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
         super.onCreate(savedInstanceState);
+        //解决net::ERR_UNKNOWN_URL_SCHEME错误
+        WebViewClient webViewClient = new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView wv, String url) {
+                if (url == null)
+                    return false;
+
+                try {
+                    if (url.startsWith("weixin://") //微信
+                            || url.startsWith("alipays://") //支付宝
+                            || url.startsWith("mailto://") //邮件
+                            || url.startsWith("tel://")//电话
+                            || url.startsWith("dianping://")//大众点评
+                            || url.startsWith("bilibili://")//哔哩哔哩
+                        //其他自定义的scheme
+                    ) {
+                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                        startActivity(intent);
+                        return true;
+                    }
+                } catch (Exception e) { //防止crash (如果手机上没有安装处理某个scheme开头的url的APP, 会导致crash)
+                    return true;//没有安装该app时，返回true，表示拦截自定义链接，但不跳转，避免弹出上面的错误页面
+                }
+
+                //处理http和https开头的url
+                wv.loadUrl(url);
+                return true;
+            }
+        };
         setContentView(R.layout.activity_web);
         Intent intent = getIntent();
         String url = intent.getStringExtra("url");
         WebView webview = findViewById(R.id.webview);
         webview.getSettings().setJavaScriptEnabled(true);
-        webview.setWebViewClient(new WebViewClient());
+        webview.setWebViewClient(webViewClient);
         webview.loadUrl(url);
     }
 }
